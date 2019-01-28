ac54a07fc1a3b68b3993ee5d04d57c5a43440ddd
Andrew LeFevre <jalefevre@liberty.edu>
2019-01-28 17:15:04 -0500

    Separate PE writing logic into two functions, fix cert writing (again)

--- pe/write.go
+++ pe/write.go
@@ -1,58 +1,55 @@
 package pe
 
 import (
-	"bufio"
+	"bytes"
 	"encoding/binary"
 	"errors"
 	"os"
 )
 
-func (peFile *File) Write(destFile string) error {
-
-	bytesWritten := uint64(0)
-	f, err := os.Create(destFile)
-	if err != nil {
-		return err
-	}
-	defer f.Close()
-	w := bufio.NewWriter(f)
+func (peFile *File) Bytes() ([]byte, error) {
+	var bytesWritten uint64
+	peBuf := bytes.NewBuffer(nil)
 
 	// write DOS header and stub
-	binary.Write(w, binary.LittleEndian, peFile.DosHeader)
+	binary.Write(peBuf, binary.LittleEndian, peFile.DosHeader)
 	bytesWritten += uint64(binary.Size(peFile.DosHeader))
-	binary.Write(w, binary.LittleEndian, peFile.DosStub)
+	binary.Write(peBuf, binary.LittleEndian, peFile.DosStub)
 	bytesWritten += uint64(binary.Size(peFile.DosStub))
 
 	// write Rich header
 	if peFile.RichHeader != nil {
-		binary.Write(w, binary.LittleEndian, peFile.RichHeader)
+		binary.Write(peBuf, binary.LittleEndian, peFile.RichHeader)
 		bytesWritten += uint64(len(peFile.RichHeader))
 	}
 
 	// apply padding before PE header if necessary
 	if uint32(bytesWritten) != peFile.DosHeader.AddressOfNewExeHeader {
 		padding := make([]byte, peFile.DosHeader.AddressOfNewExeHeader - uint32(bytesWritten))
-		binary.Write(w, binary.LittleEndian, padding)
+		binary.Write(peBuf, binary.LittleEndian, padding)
 		bytesWritten += uint64(len(padding))
 	}
 
 	// write PE header
 	peMagic := []byte{'P', 'E', 0x00, 0x00}
-	binary.Write(w, binary.LittleEndian, peMagic)
-	binary.Write(w, binary.LittleEndian, peFile.FileHeader)
+	binary.Write(peBuf, binary.LittleEndian, peMagic)
+	binary.Write(peBuf, binary.LittleEndian, peFile.FileHeader)
 	bytesWritten += uint64(binary.Size(peFile.FileHeader) + len(peMagic))
 
+	var is32bit bool
 	switch peFile.FileHeader.Machine {
 	case IMAGE_FILE_MACHINE_I386:
+		is32bit = true
 		optionalHeader := peFile.OptionalHeader.(*OptionalHeader32)
-		binary.Write(w, binary.LittleEndian, optionalHeader)
+		binary.Write(peBuf, binary.LittleEndian, peFile.OptionalHeader.(*OptionalHeader32))
 		bytesWritten += uint64(binary.Size(optionalHeader))
 	case IMAGE_FILE_MACHINE_AMD64:
+		is32bit = false
 		optionalHeader := peFile.OptionalHeader.(*OptionalHeader64)
-		binary.Write(w, binary.LittleEndian, optionalHeader)
+		binary.Write(peBuf, binary.LittleEndian, optionalHeader)
 		bytesWritten += uint64(binary.Size(optionalHeader))
 	default:
-		return errors.New("architecture not supported")
+		return nil, errors.New("architecture not supported")
 	}
 
 	// write section headers
@@ -73,7 +70,7 @@ func (peFile *File) Write(destFile string) error {
 		}
 		sectionHeaders[idx] = sectionHeader
 
-		binary.Write(w, binary.LittleEndian, sectionHeader)
+		binary.Write(peBuf, binary.LittleEndian, sectionHeader)
 		bytesWritten += uint64(binary.Size(sectionHeader))
 	}
 
@@ -81,7 +78,7 @@ func (peFile *File) Write(destFile string) error {
 	for idx, sectionHeader := range sectionHeaders {
 		sectionData, err := peFile.Sections[idx].Data()
 		if err != nil {
-			return err
+			return nil, err
 		}
 
 		// pad section if there is a gap between PointerToRawData end of last section
@@ -91,16 +88,16 @@ func (peFile *File) Write(destFile string) error {
 			sectionData = append(padding, sectionData...)
 		}
 
-		binary.Write(w, binary.LittleEndian, sectionData)
+		binary.Write(peBuf, binary.LittleEndian, sectionData)
 		bytesWritten += uint64(len(sectionData))
 	}
 
 	// write symbols
-	binary.Write(w, binary.LittleEndian, peFile.COFFSymbols)
+	binary.Write(peBuf, binary.LittleEndian, peFile.COFFSymbols)
 	bytesWritten += uint64(binary.Size(peFile.COFFSymbols))
 
 	// write the string table
-	binary.Write(w, binary.LittleEndian, peFile.StringTable)
+	binary.Write(peBuf, binary.LittleEndian, peFile.StringTable)
 	bytesWritten += uint64(binary.Size(peFile.StringTable))
 
 	var certTableOffset, certTableSize uint32
@@ -114,21 +111,46 @@ func (peFile *File) Write(destFile string) error {
 		certTableSize = 0
 	}
 
-	switch peFile.FileHeader.Machine {
-	case IMAGE_FILE_MACHINE_I386:
-		peFile.OptionalHeader.(*OptionalHeader32).DataDirectory[CERTIFICATE_TABLE].VirtualAddress = certTableOffset
-		peFile.OptionalHeader.(*OptionalHeader32).DataDirectory[CERTIFICATE_TABLE].Size = certTableSize
-	case IMAGE_FILE_MACHINE_AMD64:
-		peFile.OptionalHeader.(*OptionalHeader64).DataDirectory[CERTIFICATE_TABLE].VirtualAddress = certTableOffset
-		peFile.OptionalHeader.(*OptionalHeader64).DataDirectory[CERTIFICATE_TABLE].Size = certTableSize
-	default:
-		return errors.New("architecture not supported")
+	var certTableLoc int64
+	if is32bit {
+		certTableLoc = int64(peFile.DosHeader.AddressOfNewExeHeader) + 24 + 128
+	} else {
+		certTableLoc = int64(peFile.DosHeader.AddressOfNewExeHeader) + 24 + 144
 	}
 
-	binary.Write(w, binary.LittleEndian, peFile.CertificateTable)
+	binary.Write(peBuf, binary.LittleEndian, peFile.CertificateTable)
 	bytesWritten += uint64(len(peFile.CertificateTable))
 
-	w.Flush()
+	peData := peBuf.Bytes()
+	certTableInfo := &DataDirectory{
+		VirtualAddress: certTableOffset,
+		Size:           certTableSize,
+	}
+
+	// write the offset and size of the new Certificate Table
+	var certTableInfoBuf bytes.Buffer
+	binary.Write(&certTableInfoBuf, binary.LittleEndian, certTableInfo)
+	peData = append(peData[:certTableLoc], append(certTableInfoBuf.Bytes(), peData[int(certTableLoc) + binary.Size(certTableInfo):]...)...)
+
+	return peData, nil
+}
+
+func (peFile *File) WriteFile(destFile string) error {
+	f, err := os.Create(destFile)
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+
+	peData, err := peFile.Bytes()
+	if err != nil {
+		return err
+	}
+
+	_, err = f.Write(peData)
+	if err != nil {
+		return err
+	}
 
 	return nil
 }
